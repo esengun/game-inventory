c8547de543c42570f7bddaecac928e7a863f3f80
Mustafa Esengün <m.esengun@hotmail.com>
2024-04-17 11:57:43 +0200

    defined catalog item DTOs

---
+++ src/Play.Catalog.Service/Dtos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Play.Catalog.Service.Dtos
+{
+	public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+
+	// Id and CreatedDate are automatically populated.
+	public record CreateItemDto(string Name, string Description, decimal Price);
+
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+}
