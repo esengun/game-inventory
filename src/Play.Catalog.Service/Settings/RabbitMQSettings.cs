5939fe050de8472fe840e1e8f05bb9d18f1ae689
Mustafa Esengün <m.esengun@hotmail.com>
2024-04-27 15:52:31 +0200

    moved rabbitmq settings to common dll

--- src/Play.Catalog.Service/Settings/RabbitMQSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Play.Catalog.Service.Settings
-{
-	public class RabbitMQSettings
-	{
-		public string Host {  get; init; }
-	}
-}
--- src/Play.Catalog.Service/Startup.cs
+++ src/Play.Catalog.Service/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Settings;
+using Play.Common.MassTransit;
 using Play.Common.MongoDB;
 using Play.Common.Settings;
 
@@ -28,19 +28,10 @@ namespace Play.Catalog.Service
 			serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
             services.AddMongo()
-                .AddMongoRepository<Item>("items");
+                .AddMongoRepository<Item>("items")
+                .AddMassTransitWithRabbitMq();
 
-            services.AddMassTransit(x =>
-            {
-                x.UsingRabbitMq((context, configurator) =>
-                {
-                    var rabbitMQSettings = Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-                    configurator.Host(rabbitMQSettings.Host);
-                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-                });
-            });
-
-			services.AddControllers(options =>
+            services.AddControllers(options =>
             {
                 options.SuppressAsyncSuffixInActionNames = false;
             });
