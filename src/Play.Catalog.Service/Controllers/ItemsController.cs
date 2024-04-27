c08f7e93064143be852fd1d221092756e40deb3a
Mustafa Esengün <m.esengun@hotmail.com>
2024-04-27 10:19:51 +0200

    removed common dll related code

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Repositories;
+using Play.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
