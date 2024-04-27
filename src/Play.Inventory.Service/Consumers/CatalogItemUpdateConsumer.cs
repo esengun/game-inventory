d4d40638d8d9cf7165fefa71f8f767ad3d214a12
Mustafa Esengün <m.esengun@hotmail.com>
2024-04-27 18:32:18 +0200

    added consumers for getting updates from catalog service and store it in catalogitems collection in inventory service

---
+++ src/Play.Inventory.Service/Consumers/CatalogItemUpdateConsumer.cs
@@ -0,0 +1,42 @@
+﻿using MassTransit;
+using Play.Catalog.Contracts;
+using Play.Common;
+using Play.Inventory.Service.Entities;
+using System.Threading.Tasks;
+
+namespace Play.Inventory.Service.Consumers
+{
+	public class CatalogItemUpdateConsumer : IConsumer<CatalogItemUpdated>
+	{
+		private readonly IRepository<CatalogItem> repository;
+
+		public CatalogItemUpdateConsumer(IRepository<CatalogItem> repository)
+		{
+			this.repository = repository;
+		}
+
+		public async Task Consume(ConsumeContext<CatalogItemUpdated> context)
+		{
+			var message = context.Message;
+
+			var item = await repository.GetAsync(message.ItemId);
+
+			if (item == null)
+			{
+				item = new CatalogItem
+				{
+					Id = message.ItemId,
+					Name = message.Name,
+					Description = message.Description
+				};
+			}
+			else
+			{
+				item.Name = message.Name;
+				item.Description = message.Description;
+
+				await repository.UpdateAsync(item);
+			}
+		}
+	}
+}
