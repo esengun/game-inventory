6a44b90e43bae37bd462fbe9c89fb828f3b6b2ce
Mustafa Esengün <m.esengun@hotmail.com>
2024-04-27 22:55:28 +0200

    added cors configs to communicate with the frontend app by sending urls in headers of request and respons

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -17,7 +17,9 @@ namespace Play.Inventory.Service
 {
 	public class Startup
     {
-        public Startup(IConfiguration configuration)
+		private const string AllowedOriginSetting = "AllowedOrigin";
+
+		public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -49,7 +51,14 @@ namespace Play.Inventory.Service
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Service v1"));
-            }
+
+				app.UseCors(builder =>
+				{
+					builder.WithOrigins(Configuration[AllowedOriginSetting])
+						.AllowAnyHeader()
+						.AllowAnyMethod();
+				});
+			}
 
             app.UseHttpsRedirection();
 
