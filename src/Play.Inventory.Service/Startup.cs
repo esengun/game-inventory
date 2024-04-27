43c12f57d2090200fcc16dd282d7dc3de082d4ed
Mustafa Esengün <m.esengun@hotmail.com>
2024-04-27 13:50:20 +0200

    added exponential backoff timeout handling for communication with catalog service

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -8,6 +8,7 @@ using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 using System;
 using System.Net.Http;
 
@@ -28,10 +29,21 @@ namespace Play.Inventory.Service
             services.AddMongo()
                 .AddMongoRepository<InventoryItem>("inventoryitems");
 
+            Random jitterer = new Random();
+
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new Uri("https://localhost:44376");
             })
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                    5, 
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                                    + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+                    onRetry: (outcome, timespan, retryattempt) =>
+                    {
+                        Console.WriteLine($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryattempt}");
+                    }
+                    ))
                 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
             services.AddControllers();
